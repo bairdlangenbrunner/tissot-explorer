e8f6117d4958a16886282b35b5fddfe258666c24
Baird Langenbrunner <bairdlangenbrunner@gmail.com>
2026-04-02 01:40:00 -0400

    adding in d3 prjecions

--- src/d3-geo-projection.d.ts
+++ src/d3-geo-projection.d.ts
@@ -1,9 +1,28 @@
 declare module 'd3-geo-projection' {
   import type { GeoProjection } from 'd3-geo';
-  export function geoMollweide(): GeoProjection;
-  export function geoRobinson(): GeoProjection;
-  export function geoEckert4(): GeoProjection;
-  export function geoSinusoidal(): GeoProjection;
+  export function geoAitoff(): GeoProjection;
+  export function geoArmadillo(): GeoProjection;
+  export function geoAugust(): GeoProjection;
+  export function geoBaker(): GeoProjection;
   export function geoBerghaus(): GeoProjection;
+  export function geoBoggs(): GeoProjection;
+  export function geoBottomley(): GeoProjection;
+  export function geoCollignon(): GeoProjection;
+  export function geoEckert4(): GeoProjection;
+  export function geoEckert6(): GeoProjection;
+  export function geoEisenlohr(): GeoProjection;
+  export function geoGingery(): GeoProjection;
+  export function geoHammer(): GeoProjection;
   export function geoInterruptedHomolosine(): GeoProjection;
+  export function geoKavrayskiy7(): GeoProjection;
+  export function geoLagrange(): GeoProjection;
+  export function geoLoximuthal(): GeoProjection;
+  export function geoMollweide(): GeoProjection;
+  export function geoPatterson(): GeoProjection;
+  export function geoPeirceQuincuncial(): GeoProjection;
+  export function geoPolyconic(): GeoProjection;
+  export function geoRobinson(): GeoProjection;
+  export function geoSinusoidal(): GeoProjection;
+  export function geoVanDerGrinten(): GeoProjection;
+  export function geoWinkel3(): GeoProjection;
 }
